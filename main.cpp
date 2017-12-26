a032d87350f5bed6ee4a67111f7ab65ac1ee1815
KekKekovich <Ilyavikt-95@mail.ru>
2017-12-27 01:11:54 +0300

    message

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-const int N=1000;
+const int N=100;
 double mu = 0;
 double sigma = 10;
 double sum;
@@ -16,7 +16,7 @@ double sum1;
 double Gauss(double mu, double sigma, double x) {
     return (1/sigma/(sqrt(2*3.14)))*exp(-(x-mu)*(x-mu)/2/sigma/sigma);
 }
-double Randd (double fMin, double fMax){
+double Rand (double fMin, double fMax){
     double f=(double)rand()/ RAND_MAX;
     return fMin+f*(fMax-fMin);
 }
@@ -31,56 +31,38 @@ int main() {
         X.push_back(i);
     }
 
-
-
-    for (double i=0; i<A.size(); i++) {
-        cout <<"density: " << A[i] << endl;
+    for (auto &x: A) {
+        sum+=x;
     }
-    for (double i=0; i<A.size(); i++) {
-        sum+=A[i];
-    }
-    cout << "Summa bez normirovki: "<<sum << endl;
-
-
+    //cout << "Summa bez normirovki: "<<sum << endl;
 
     map<double,double > map1;
     for (double i=0; i<A.size(); i++) {
         sum1+=(A[i]/sum);
         map1.emplace(sum1, X[i]);
-        cout <<"   "<<sum1 << endl;
+        //cout <<sum1 << " | " << X[i]<< endl;
     }
     cout<<endl;
 
     vector <double> POINTS;
-
     for (int i = 0;i<N;i++) {
-        double r = Randd(0,1);
-        double xx = 0;
-        for (auto &x: map1) {
-            if (x.first > r) {
-                xx = x.second;
-                POINTS.push_back(xx);
-                break;
-            }
-        }
-        cout<<r<<"   "<<xx<<endl;
+        POINTS.push_back(map1.lower_bound(Rand(0,1)) -> second);
+        cout << POINTS[i] << ", ";
     }
+    cout << endl;
 
     double Sum = 0;
-    for (double i=0; i<POINTS.size(); i++) {
-        Sum += POINTS[i];
+    for (auto &x: POINTS) {
+        Sum+=x;
     }
-    double mean = Sum/POINTS.size();
-    cout << mean << endl;
+    double mean = Sum / N;
+    cout <<"MU: " <<mean << endl;
 
     double var;
-    for (double i=0; i<POINTS.size(); i++) {
-        var+= ((POINTS[i]-mean)*(POINTS[i]-mean));
-
+    for (auto &x: POINTS) {
+        var+=(x-mean)*(x-mean);
     }
-    cout << var/(POINTS.size()-1) << endl;
-
-
+    cout <<"SIGMAKVADRAT:" <<var/(POINTS.size()-1) << endl;
 
     return 0;
 }
